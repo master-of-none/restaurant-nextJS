bff0c3e36dff061d8820952810402eb3f66b8f7d
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-01 16:23:20 +0530

    single product initial commit

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Image from "next/image";
+import { singleProduct } from "@/data";
 
 const SingleProduct = () => {
-    return <div>SingleProduct</div>;
+    return (
+        <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row">
+            {/* Image Container  */}
+            {singleProduct.img && (
+                <div className="relative">
+                    <Image
+                        src={singleProduct.img}
+                        alt="Image"
+                        className="object-contain"
+                        fill
+                    />
+                </div>
+            )}
+            {/* Text Container  */}
+            <div className="">
+                <h1>{singleProduct.title}</h1>
+                <p>{singleProduct.desc}</p>
+            </div>
+        </div>
+    );
 };
 
 export default SingleProduct;
