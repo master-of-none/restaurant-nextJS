e0572e40274bf25c88cbef6398d71c211f2e049a
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-01 18:21:50 +0530

    Product - done

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Image from "next/image";
 import { singleProduct } from "@/data";
+import Price from "@/components/Price";
 
 const SingleProduct = () => {
     return (
-        <div className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row">
+        <div
+            className="p-4 lg:px-20 xl:px-40 h-screen flex flex-col justify-around text-red-500 md:flex-row
+        md:gap-8 md:items-center"
+        >
             {/* Image Container  */}
             {singleProduct.img && (
-                <div className="relative">
+                <div className="relative w-full h-1/2 md:h-[70%]">
                     <Image
                         src={singleProduct.img}
                         alt="Image"
@@ -17,9 +21,16 @@ const SingleProduct = () => {
                 </div>
             )}
             {/* Text Container  */}
-            <div className="">
-                <h1>{singleProduct.title}</h1>
+            <div className="h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
+                <h1 className="text-3xl font-bold uppercase xl:text-5xl">
+                    {singleProduct.title}
+                </h1>
                 <p>{singleProduct.desc}</p>
+                <Price
+                    price={singleProduct.price}
+                    id={singleProduct.id}
+                    options={singleProduct.options}
+                />
             </div>
         </div>
     );
