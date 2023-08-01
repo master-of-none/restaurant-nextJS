d939d0e5e7bda0fd6f68f017e98037fbf48067fd
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-08-01 10:56:48 +0530

    Added some components

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import CartIcon from "./CartIcon";
 
 const links = [
     { id: 1, title: "Homepage", url: "/" },
@@ -12,6 +13,8 @@ const links = [
 ];
 const Menu = () => {
     const [open, setOpen] = useState(false);
+    // Temporary
+    const user = false;
     return (
         <div>
             {!open ? (
@@ -31,16 +34,34 @@ const Menu = () => {
                     onClick={() => setOpen(false)}
                 />
             )}
-            <div
-                className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] 
+            {open && (
+                <div
+                    className="bg-red-500 text-white absolute left-0 top-24 w-full h-[calc(100vh-6rem)] 
             flex flex-col gap-8 items-center justify-center text-3xl z-10"
-            >
-                {links.map((item) => (
-                    <Link href={item.url} key={item.id}>
-                        {item.title}
+                >
+                    {links.map((item) => (
+                        <Link
+                            href={item.url}
+                            key={item.id}
+                            onClick={() => setOpen(false)}
+                        >
+                            {item.title}
+                        </Link>
+                    ))}
+                    {!user ? (
+                        <Link href="/login" onClick={() => setOpen(false)}>
+                            Login
+                        </Link>
+                    ) : (
+                        <Link href="/orders" onClick={() => setOpen(false)}>
+                            Orders
+                        </Link>
+                    )}
+                    <Link href="/cart" onClick={() => setOpen(false)}>
+                        <CartIcon />
                     </Link>
-                ))}
-            </div>
+                </div>
+            )}
         </div>
     );
 };
